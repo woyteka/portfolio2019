985b4ee9348fb85c25a684a75698ec42c1631ff9
woyteka <40150050+woyteka@users.noreply.github.com>
2018-11-20 10:53:43 +0100

    A01

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    
+    $(".box-button-menu").click(function(){ 
+         $(".three").toggleClass("rotthree");
+        $(".one").toggleClass("rotone");
+         $(".two").toggleClass("opacitytwo");
+        $(".menu-line").toggleClass("color");
+      $(".menu-roll-container").toggleClass("show");
+       
+             
+});
+    
+});
